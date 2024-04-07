55991ac027d5c2c5b090d5f2d4ce5291351e2d53
ivan grachev <ivan@ivan.lol>
2024-04-07 23:39:59 +0300

    setup based on app-response type

--- web/src/app/dashboard/dashboard-feature.tsx
+++ web/src/app/dashboard/dashboard-feature.tsx
@@ -1,6 +1,6 @@
 import { AppHero } from '../ui/ui-layout';
 import { useLocation } from 'react-router-dom';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useWallet } from '@solana/wallet-adapter-react';
 import { WalletButton } from '../solana/solana-provider';
 
@@ -19,27 +19,51 @@ export default function DashboardFeature() {
 
   const [pageTitle, setPageTitle] = useState("mons");
   const [subtitle, setSubtitle] = useState("🥱");
+  const [buttonTitle, setButtonTitle] = useState("");
   const [heroBgColor, setHeroBgColor] = useState("");
   const [redirectCount, setRedirectCount] = useState(0);
 
+  useEffect(() => {
+    switch (queryPairs["type"]) {
+      case "createSecretInvite":
+        setPageTitle("create onchain match");
+        setSubtitle("🪙");
+        setButtonTitle("0.042 sol");
+        break;
+      case "acceptSecretInvite":
+        setPageTitle("join onchain match");
+        setSubtitle("🪙");
+        setButtonTitle("0.042 sol");
+        break;
+      default:
+        setPageTitle(redirectCount === 0 ? "mons" : "redirected");
+        setSubtitle(redirectCount === 0 ? "🥱" : "make sure you have mons app installed");
+        setButtonTitle(redirectCount === 0 ? "new match" : "get mons app");
+        break;
+    }
+  }, [queryPairs, redirectCount]);
+
   const handleRedirect = () => {
-    if (redirectCount === 0) {
-      const newTitle = "redirected";
+    if (redirectCount != 0) {
+      window.location.href = "https://mons.link";
+      return;
+    } else if (queryPairs["type"] === "createSecretInvite") {
+      return;
+    } else if (queryPairs["type"] === "acceptSecretInvite") {
+      return;
+    } else {
+      const newTitle = "🟩 redirected";
       document.title = newTitle;
-      setPageTitle(newTitle);
       setHeroBgColor("green");
-      setSubtitle("make sure you have mons app installed");
       window.location.href = `supermons://?type=createSecretInvite`;
       setRedirectCount(1);
-    } else {
-      window.location.href = "https://mons.link";
     }
   };
 
   return publicKey ? (
     <div style={{backgroundColor: heroBgColor}}>
       <AppHero title={pageTitle} subtitle={subtitle}>
-        <button className="btn btn-primary" onClick={handleRedirect}>{redirectCount === 0 ? "new match" : "get mons app"}</button>
+        <button className="btn btn-primary" onClick={handleRedirect}>{buttonTitle}</button>
         <div>
         <p><br /><br /></p>
           {Object.entries(queryPairs).map(([key, value]) => (
