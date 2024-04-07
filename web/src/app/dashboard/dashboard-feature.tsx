3df91ddc9afe61641759c96cfbd01ab21c21bc44
ivan grachev <ivan@ivan.lol>
2024-04-07 15:09:49 +0300

    new match button

--- web/src/app/dashboard/dashboard-feature.tsx
+++ web/src/app/dashboard/dashboard-feature.tsx
@@ -3,7 +3,11 @@ import { AppHero } from '../ui/ui-layout';
 export default function DashboardFeature() {
   return (
     <div>
-      <AppHero title="mons" subtitle="wip" />
+      <AppHero title="mons" subtitle="wip">
+        <a href="https://mons.rehab/app-request?type=createSecretInvite" target="_blank" rel="noopener noreferrer">
+          <button className="btn btn-primary">new match</button>
+        </a>
+      </AppHero>
     </div>
   );
 }
