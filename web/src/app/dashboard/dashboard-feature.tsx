10ec3140b34fc65c0b0f78a187145c812c07f5f4
ivan grachev <ivan@ivan.lol>
2024-04-07 18:51:46 +0300

    show query at root

--- web/src/app/dashboard/dashboard-feature.tsx
+++ web/src/app/dashboard/dashboard-feature.tsx
@@ -1,12 +1,17 @@
 import { AppHero } from '../ui/ui-layout';
+import { useLocation } from 'react-router-dom';
 
 export default function DashboardFeature() {
+  const location = useLocation();
+  const fullQuery = location.search;
+
   return (
     <div>
       <AppHero title="mons" subtitle="wip">
-        <a href="https://mons.rehab/app-request?type=createSecretInvite" target="_blank" rel="noopener noreferrer">
+        <a href={`https://mons.rehab/app-request?type=createSecretInvite`} target="_blank" rel="noopener noreferrer">
           <button className="btn btn-primary">new match</button>
         </a>
+        <p><br />{fullQuery}</p>
       </AppHero>
     </div>
   );
