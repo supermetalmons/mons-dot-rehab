9edf9de781dc511e9c4cfce70e7b0a31195b9fd8
ivan grachev <ivan@ivan.lol>
2024-04-07 21:24:51 +0300

    fallback to app store from redirect page

--- web/src/app/dashboard/dashboard-feature.tsx
+++ web/src/app/dashboard/dashboard-feature.tsx
@@ -9,20 +9,26 @@ export default function DashboardFeature() {
   const [pageTitle, setPageTitle] = useState("mons");
   const [subtitle, setSubtitle] = useState("🥱");
   const [heroBgColor, setHeroBgColor] = useState("");
+  const [redirectCount, setRedirectCount] = useState(0);
 
-  const changePageTitleAndBgColor = () => {
-    const newTitle = "redirected";
-    document.title = newTitle;
-    setPageTitle(newTitle);
-    setHeroBgColor("green");
-    setSubtitle("make sure you have mons app installed");
-    window.location.href = `supermons://?type=createSecretInvite`;
+  const handleRedirect = () => {
+    if (redirectCount === 0) {
+      const newTitle = "redirected";
+      document.title = newTitle;
+      setPageTitle(newTitle);
+      setHeroBgColor("green");
+      setSubtitle("make sure you have mons app installed");
+      window.location.href = `supermons://?type=createSecretInvite`;
+      setRedirectCount(1);
+    } else {
+      window.location.href = "https://mons.link";
+    }
   };
 
   return (
     <div style={{backgroundColor: heroBgColor}}>
       <AppHero title={pageTitle} subtitle={subtitle}>
-        <button className="btn btn-primary" onClick={changePageTitleAndBgColor}>new match</button>
+        <button className="btn btn-primary" onClick={handleRedirect}>{redirectCount === 0 ? "new match" : "get mons app"}</button>
         <p><br />{fullQuery}</p>
       </AppHero>
     </div>
