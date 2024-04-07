edc28dd46d096c33e2f18112ad6f8c7c524fb699
ivan grachev <ivan@ivan.lol>
2024-04-07 20:51:06 +0300

    use deeplink

--- web/src/app/dashboard/dashboard-feature.tsx
+++ web/src/app/dashboard/dashboard-feature.tsx
@@ -8,7 +8,7 @@ export default function DashboardFeature() {
   return (
     <div>
       <AppHero title="mons" subtitle="wip">
-        <a href={`https://mons.rehab/app-request?type=createSecretInvite`} target="_blank" rel="noopener noreferrer">
+        <a href={`supermons://?type=createSecretInvite`}>
           <button className="btn btn-primary">new match</button>
         </a>
         <p><br />{fullQuery}</p>
