343e61ebf72692aced2c04b4cd862ed501fe3bd5
ivan grachev <ivan@ivan.lol>
2024-04-07 21:08:27 +0300

    colorful redirect

--- web/src/app/dashboard/dashboard-feature.tsx
+++ web/src/app/dashboard/dashboard-feature.tsx
@@ -1,24 +1,26 @@
 import { AppHero } from '../ui/ui-layout';
 import { useLocation } from 'react-router-dom';
-import { useState } from 'react'; // Added import for useState
+import { useState } from 'react';
 
 export default function DashboardFeature() {
   const location = useLocation();
   const fullQuery = location.search;
 
   const [pageTitle, setPageTitle] = useState("mons");
+  const [heroBgColor, setHeroBgColor] = useState("");
 
-  const changePageTitle = () => {
-    const newTitle = "redirect";
+  const changePageTitleAndBgColor = () => {
+    const newTitle = "redirected";
     document.title = newTitle;
     setPageTitle(newTitle);
+    setHeroBgColor("green");
     window.location.href = `supermons://?type=createSecretInvite`;
   };
 
   return (
-    <div>
+    <div style={{backgroundColor: heroBgColor}}>
       <AppHero title={pageTitle} subtitle="wip">
-        <button className="btn btn-primary" onClick={changePageTitle}>new match</button>
+        <button className="btn btn-primary" onClick={changePageTitleAndBgColor}>new match</button>
         <p><br />{fullQuery}</p>
       </AppHero>
     </div>
