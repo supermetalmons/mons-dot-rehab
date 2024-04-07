09e4bfb9e31d4fdc505f7ae7b8f075bbbfb7fb66
ivan grachev <ivan@ivan.lol>
2024-04-07 21:15:08 +0300

    tune subtitle

--- web/src/app/dashboard/dashboard-feature.tsx
+++ web/src/app/dashboard/dashboard-feature.tsx
@@ -7,6 +7,7 @@ export default function DashboardFeature() {
   const fullQuery = location.search;
 
   const [pageTitle, setPageTitle] = useState("mons");
+  const [subtitle, setSubtitle] = useState("🥱");
   const [heroBgColor, setHeroBgColor] = useState("");
 
   const changePageTitleAndBgColor = () => {
@@ -14,12 +15,13 @@ export default function DashboardFeature() {
     document.title = newTitle;
     setPageTitle(newTitle);
     setHeroBgColor("green");
+    setSubtitle("make sure you have mons app installed");
     window.location.href = `supermons://?type=createSecretInvite`;
   };
 
   return (
     <div style={{backgroundColor: heroBgColor}}>
-      <AppHero title={pageTitle} subtitle="wip">
+      <AppHero title={pageTitle} subtitle={subtitle}>
         <button className="btn btn-primary" onClick={changePageTitleAndBgColor}>new match</button>
         <p><br />{fullQuery}</p>
       </AppHero>
